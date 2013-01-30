9142cba8ab086d315cddc975c5062dca65e9eb4e
Stephen Gowan <gowansg@gmail.com>
2013-01-29 22:36:03 -0500

    added custom generator to downcase all post names

---
+++ _plugins/DowncaseGenerator.rb
@@ -0,0 +1,10 @@
+module Jekyll
+	class DowncasedPostNameGenerator < Generator
+		safe true		
+		priority :low
+
+		def generate(site)
+			site.posts.each{ |post| puts post.url.downcase! }
+		end
+	end
+end 
